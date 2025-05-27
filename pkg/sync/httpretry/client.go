65fd50b3ce23ac77429422aff5c5d15b609fbc45
moshe-blox <moshe@ssvlabs.io>
2025-05-27 11:27:04 +0300

    Increase max retry count to 10 and add handling for 429 status code in HTTP retry policy

--- pkg/sync/httpretry/client.go
+++ pkg/sync/httpretry/client.go
@@ -7,11 +7,11 @@ import (
 )
 
 var Client = httpretry.NewDefaultClient(
-	httpretry.WithMaxRetryCount(5),
+	httpretry.WithMaxRetryCount(10),
 
 	// Retry on any error, 5xx status codes and 0 status codes.
 	httpretry.WithRetryPolicy(func(statusCode int, err error) bool {
-		return err != nil || statusCode >= 500 || statusCode == 0
+		return err != nil || statusCode >= 500 || statusCode == 0 || statusCode == 429
 	}),
 
 	// Retry with an incremental backoff policy.
