62ab1686e43ec5fb1992be2063d3f281ea4e2c35
olegshmuelov <oleg@ssvlabs.io>
2025-09-01 11:41:49 +0300

    use Gwei in calculation

--- cmd/ssv-rewards/calc.go
+++ cmd/ssv-rewards/calc.go
@@ -519,7 +519,7 @@ func (c *CalcCmd) processRoundLegacy(
 			participation.RegisteredDays,
 			roundDays,
 			dailyReward,
-			networkFee.Wei(),
+			networkFee.Gwei(),
 		)
 		if err != nil {
 			return nil, fmt.Errorf("failed to calculate validator reward: %w", err)
@@ -533,7 +533,7 @@ func (c *CalcCmd) processRoundLegacy(
 			participation.RegisteredDays,
 			roundDays,
 			dailyReward,
-			networkFee.Wei(),
+			networkFee.Gwei(),
 		)
 		if err != nil {
 			return nil, fmt.Errorf("failed to calculate owner reward: %w", err)
@@ -547,7 +547,7 @@ func (c *CalcCmd) processRoundLegacy(
 			participation.RegisteredDays,
 			roundDays,
 			dailyReward,
-			networkFee.Wei(),
+			networkFee.Gwei(),
 		)
 		if err != nil {
 			return nil, fmt.Errorf("failed to calculate recipient reward: %w", err)
